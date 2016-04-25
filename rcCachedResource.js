f1c610ba1e2790ec21cdb8910f0605dafbbf031e
Rafael Thomas Goz Coutinho <rafael.coutinho@gmail.com>
2016-04-25 16:40:12 -0300

    Add Debug to avoid logging

--- rcCachedResource.js
+++ rcCachedResource.js
@@ -12,54 +12,59 @@
     
 } (this, function (angular) {
     'use strict';
+    var _DEBUG = false;
     function isFunction(functionToCheck) {
         var getType = {};
         return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
     }
     function CacheEntry(url, params, actions, cacheParams, $localStorage, $q, $resource, $log, $timeout) {
-        this.res = $resource(url,params,actions);
-        this.cacheTimeout = cacheParams.cacheTimeout*1000*60;//minutes
-        if(!this.cacheTimeout){
-            this.cacheTimeout=-1;
+        this.res = $resource(url, params, actions);
+        this.cacheTimeout = cacheParams.cacheTimeout * 1000 * 60;//minutes
+        if (!this.cacheTimeout) {
+            this.cacheTimeout = -1;
         }
-        this.cacheHalfLife=cacheParams.cacheHalfLife;
-        
+        this.cacheHalfLife = cacheParams.cacheHalfLife;
+
         this.name = cacheParams.name;
-        if(this.name==null||this.name.length==0){
+        if (this.name == null || this.name.length == 0) {
             throw "Cache name is mandatory";
         }
-      
-         
-         var forEach = angular.forEach;
-         var me = this;
-         if(!$localStorage[this.name]){
-             $log.debug("creating cached resource",url,this.name,this.cacheTimeout);
-             $localStorage[me.name]={};
-         }
-         this.clear = function(){
-		     $log.log("Limpando cache",me.name);
-             delete $localStorage[me.name];
-             $localStorage[me.name]={};
-         }
-         if(!actions["get"]){
-             actions["get"]={method: 'GET'};
-         }
-         if(!actions["query"]){
-             actions["query"]={method: 'GET', isArray: true};
-         }
+
+
+        var forEach = angular.forEach;
+        var me = this;
+        if (!$localStorage[this.name]) {
+            if (_DEBUG) {
+                $log.debug("creating cached resource", url, this.name, this.cacheTimeout);
+            }
+            $localStorage[me.name] = {};
+        }
+        this.clear = function () {
+            if (_DEBUG) {
+                $log.log("Limpando cache", me.name);
+            }
+            delete $localStorage[me.name];
+            $localStorage[me.name] = {};
+        }
+        if (!actions["get"]) {
+            actions["get"] = { method: 'GET' };
+        }
+        if (!actions["query"]) {
+            actions["query"] = { method: 'GET', isArray: true };
+        }
          
          
          forEach(actions, function (action, nameFct) {
              me[nameFct] = function (a, b) {
-                   if(isFunction(a)==true){
-                       $log.info("param a is a function");
-                   }
-                   if(isFunction(b)==true){
-                       $log.info("param b is a function");
-                   }
+                 if (isFunction(a) == true) {
+                     $log.warn("param a is a function");
+                 }
+                 if (isFunction(b) == true) {
+                     $log.warn("param b is a function");
+                 }
                  var deferred = $q.defer();
-                 if ( nameFct!='get' && nameFct!='query' && action.method != 'GET') {
-                     $log.info("NOT a GET method, execute action ", action,"name", nameFct);
+                 if (nameFct != 'get' && nameFct != 'query' && action.method != 'GET') {
+                     $log.error("NOT a GET method, execute action ", action, "name", nameFct);
                      me.res[nameFct](a, b,
                          function (data) {
                              deferred.resolve(data);
@@ -86,64 +91,84 @@
                  var checkValidity = function (cacheEntry) {
                      var timeout = me.cacheTimeout;
                      if (cacheParams.isCacheValid) {
-                        //  $log.debug("   using cr_isCacheValid function timeout");
+                         if (_DEBUG) {
+                             $log.debug("   using cr_isCacheValid function timeout");
+                         }
                          return cacheParams.isCacheValid(cacheEntry, params);
                      } else if (cacheParams.timeout) {
-                        //  $log.debug("   using actions timeout", (cacheParams.timeout/1000)+"s");
+                         if (_DEBUG) {
+                             $log.debug("   using actions timeout", (cacheParams.timeout / 1000) + "s");
+                         }
                          timeout = cacheParams.timeout;
                      } else {
-                        //  $log.debug("   using def constructor timeout", (timeout/1000)+"s");
+                         if (_DEBUG) {
+                             $log.debug("   using def constructor timeout", (timeout / 1000) + "s");
+                         }
                      }
 
                      return (timeout <= 0 || ((new Date().getTime() - cache.date) < timeout));
                  }
                  var reloadCache = function (executeCallback, onFailure) {
-                    //   console.log("    reloadCache ",cacheName,me.res[nameFct]);
-                     try{
-                     me.res[nameFct](a, b,
-                         function (data) {
-                            //  $log.debug("Storing '" + "("+me.name+"."+cacheName+")' ",$localStorage[me.name][cacheName])
-                             var cacheData = {
-                                 data: data,
-                                 date: new Date().getTime()
-                             }
-                             $localStorage[me.name][cacheName] = cacheData;
-                              
-                             if (executeCallback) {
-                                 deferred.resolve(data);
-                             }
-                         }, function (error) {
-                             $log.log("Failed to load from server:" + JSON.stringify(error)+" - "+url+" => "+action.url+ " params "+JSON.stringify(a)+" and "+JSON.stringify(b));
-                             if (executeCallback) {
-                                 if (onFailure != null) {
-                                      $log.log("Returning previously cached");
-                                     deferred.resolve(onFailure)
-                                 } else {
-                                     deferred.reject(error);
+
+                     try {
+                         me.res[nameFct](a, b,
+                             function (data) {
+                                 if (_DEBUG) {
+                                     $log.debug("Storing '" + "(" + me.name + "." + cacheName + ")' ", $localStorage[me.name][cacheName])
                                  }
-                             }
-                         });
-                     }catch(e){
-                         $log.log("erro ceatch " + JSON.stringify(e));
+                                 var cacheData = {
+                                     data: data,
+                                     date: new Date().getTime()
+                                 }
+                                 $localStorage[me.name][cacheName] = cacheData;
+
+                                 if (executeCallback) {
+                                     deferred.resolve(data);
+                                 }
+                             }, function (error) {
+                                 if (_DEBUG) {
+                                     $log.warn("Failed to load from server:" + JSON.stringify(error) + " - " + url + " => " + action.url + " params " + JSON.stringify(a) + " and " + JSON.stringify(b));
+                                 }
+                                 if (executeCallback) {
+                                     if (onFailure != null) {
+                                         if (_DEBUG) {
+                                             $log.log("Returning previously cached");
+                                         }
+                                         deferred.resolve(onFailure)
+                                     } else {
+                                         deferred.reject(error);
+                                     }
+                                 }
+                             });
+                     } catch (e) {
+                         $log.log("erro creating cache " + JSON.stringify(e));
                      }
                  };
                  if (cache && cache.data != null) {
                      //check if it's expired
                      if (checkValidity(cache) == true) {
-                         $log.debug("Cache hit '" + (cacheName) + "'");
+                         if (_DEBUG) {
+                             $log.debug("Cache hit '" + (cacheName) + "'");
+                         }
                          deferred.resolve(cache.data);
                          if (cacheParams.cacheHalfLife && cacheParams.cacheHalfLife(cache)) {
-                             $log.debug("Cache halflife reached");
+                             if (_DEBUG) {
+                                 $log.debug("Cache halflife reached");
+                             }
                              reloadCache(false);
                          }
                      } else {
-                          $log.debug("Cache expired '" + (cacheName) + "'");
+                         if (_DEBUG) {
+                             $log.debug("Cache expired '" + (cacheName) + "'");
+                         }
                          reloadCache(true, cache.data);
                      }
 
 
                  } else {
-                     $log.debug("Cache miss '" + (cacheName) + "'");
+                     if (_DEBUG) {
+                         $log.debug("Cache miss '" + (cacheName) + "'");
+                     }
                      reloadCache(true);
                  }
                   
@@ -165,12 +190,7 @@
 
     return angular.module('rcCachedResource', ['ngStorage', 'ngResource'])
 
-    /**
-     * @ngdoc object
-     * @name ngStorage.$localStorage
-     * @requires $rootScope
-     * @requires $window
-     */
+    
 
         .provider('$cachedResource', _cachedStorageProvider())
     function _cachedStorageProvider() {
